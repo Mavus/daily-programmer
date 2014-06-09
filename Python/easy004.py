401e6810a7c291e7d3bde369ad4af2ae0e3fb9af
Mavus <adhwhitaker@gmail.com>
2014-06-09 12:10:42 +0000

    bonus for 004 done

--- Python/easy004.py
+++ Python/easy004.py
@@ -4,8 +4,8 @@ You're challenge for today is to create a random password generator!
 For extra credit, allow the user to specify the amount of passwords to generate.
 For even more extra credit, allow the user to specify the length of the strings he wants to generate!
 [MAIN]: DONE
-[EXTRA]: TODO
-[EXTRAEXTRA]: TODO
+[EXTRA]: DONE
+[EXTRAEXTRA]: DONE
 """
 from random import randint
 
@@ -13,4 +13,10 @@ def generate_password(length=16):
     random_characters = map(chr,[randint(33,127) for i in range(length+1)])
     return ''.join(random_characters)
 
-print generate_password()
+try:
+    n = int(input("How many passwords would you like to generate?: "))
+    l = int(input("How long should each password be?: "))
+    for i in range(n):
+        print generate_password(l)
+except ValueError:
+    print "Input is not a number"
